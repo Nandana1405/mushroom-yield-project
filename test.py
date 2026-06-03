46d80fef544f2cf02fac66911a5dc2d531526769
K M Nandana <nandanakm233@gmail.com>
2026-06-03 12:44:34 +0530

    Complete task requirements

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 sensor_data = {
     "temperature": 24.5,
     "humidity": 85,
-    "co2": 650
+    "co2": 650,
+    "Yield":1.2
 }
 
 print("Sensor Readings")
 print(f"Temperature: {sensor_data['temperature']} °C")
 print(f"Humidity: {sensor_data['humidity']} %")
 print(f"CO2: {sensor_data['co2']} ppm")
+print(f"Yield: {sensor_data['Yield']} kg")

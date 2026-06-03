adb2349a87af99ede1767501d5cdb522a4a26505
K M Nandana <nandanakm233@gmail.com>
2026-06-03 11:40:43 +0530

    Initial project structure

---
+++ src/smoke_test.py
@@ -0,0 +1,10 @@
+sensor = {
+    "temperature": 24.5,
+    "humidity": 85,
+    "co2": 650
+}
+
+print("Sensor Readings")
+print(f"Temperature: {sensor['temperature']} °C")
+print(f"Humidity: {sensor['humidity']} %")
+print(f"CO2: {sensor['co2']} ppm")
